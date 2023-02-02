df3818eef7eed2006440dc19748517f2b37729b4
vnabdulov <42705658+vnabdulov@users.noreply.github.com>
2023-02-02 15:03:47 -0500

    added a word file conversion

--- apps/data_processing.py
+++ apps/data_processing.py
@@ -25,3 +25,25 @@ layout = html.Div([
 
 
 
+import pandas as pd
+from dash import dcc, Dash, html, dash_table
+import base64
+import datetime
+import re
+
+
+
+def get_data_from_word(path_to_file):
+    from docx import Document
+    pathname = directory + '/' + filename
+    # Creating a word file object
+    doc_object = open(path_to_file, "rb")
+
+    # creating word reader object
+    doc_reader = Document(doc_object)
+    data = ""
+
+    for p in doc_reader.paragraphs:
+        data += p.text + "\n"
+
+    return data
