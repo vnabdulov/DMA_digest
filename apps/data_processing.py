e67fbe2086c63b6578b55d2fa3d1398642350eff
vnabdulov <42705658+vnabdulov@users.noreply.github.com>
2023-02-04 06:57:30 -0500

    added a word file cparser

--- apps/data_processing.py
+++ apps/data_processing.py
@@ -1,49 +1,104 @@
-from dash import Dash, dcc, html
-import dash_bootstrap_components as dbc
-
-layout = html.Div([
-
-
-    dcc.Upload([
-        'Drag and Drop or ',
-        html.A('Click to Select a File'),
-
-    ],  id='interview-data',
-        style={
-        'width': 'auto',
-        'height': 'auto',
-        'lineHeight': '60px',
-        'borderWidth': '1px',
-        'borderStyle': 'dashed',
-        'borderRadius': '5px',
-        'textAlign': 'center',
-        'margin-left':'6rem',
-        'margin-right':'6rem'
-    })
-])
-
-
-
-
-import pandas as pd
-from dash import dcc, Dash, html, dash_table
 import base64
 import datetime
-import re
+import io
 
+import dash
+from dash.dependencies import Input, Output, State
+from dash import dcc, html, dash_table
 
+layout = layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+])
 
-def get_data_from_word(path_to_file):
-    from docx import Document
-    pathname = directory + '/' + filename
-    # Creating a word file object
-    doc_object = open(path_to_file, "rb")
+def parse_contents(contents, filename, date):
+    import pandas as pd
+    content_type, content_string = contents.split(',')
 
-    # creating word reader object
-    doc_reader = Document(doc_object)
-    data = ""
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+        elif 'docx' in filename:
+            import docx
+            file = io.BytesIO(decoded)
+            doc = docx.Document(file)
+            text = ""
+            for para in doc.paragraphs:
+                text += para.text+"\n"
+            try:
+                file_data = text.split('\n')
+                paragraphs = []
+                s = 0
+                f = 3
+                for x in range(len(file_data) // 3):
+                    paragraphs.append(file_data[s:f])
+                    s += 3
+                    f += 3
+                import pandas as pd
+                df = pd.DataFrame(paragraphs)
+                df.columns = ['time', 'speaker', 'text']
+            except Exception as e:
+                print(e)
+                return html.Div([
+                    'There was an error parsing this file.'
+                ])
 
-    for p in doc_reader.paragraphs:
-        data += p.text + "\n"
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
 
-    return data
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            df.to_dict('records'),
+            [{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
